e5c2aedce8a0eba37beabd43105ca5c3c24064b1
Chase <chase@rhusoft.com>
2025-11-29 16:10:57 -0800

    test will fail

--- src/test/java/org/psnbtech/util/ClockTest.java
+++ src/test/java/org/psnbtech/util/ClockTest.java
@@ -12,7 +12,7 @@ class ClockTest {
     void setCyclesPerSecond() {
         c.setCyclesPerSecond(20);
 
-        assertEquals(50.0f, c.millisPerCycle);
+        assertEquals(500.0f, c.millisPerCycle);
     }
     @Test
     void isPaused() {
